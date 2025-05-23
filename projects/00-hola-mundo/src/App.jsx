d6c0e6c2c666a6c108ddde050856ab791a893f71
Juan Sifuentes Fernandez <89866333+JuanSifuentesF@users.noreply.github.com>
2025-05-22 20:24:53 -0500

    fase1

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,19 +1,50 @@
 import './App.css'
+import './index.css'
 import {TwitterFollowCard} from "./TwitterFollowCard.jsx";
+
+const users = [
+    {
+        index: 0,
+        userName: 'midudev',
+        name: 'Miguel Angel Duran',
+        isFollowing: true
+    },
+    {
+        index: 1,
+        userName: 'pheralb',
+        name: 'Pablo Hernandez',
+        isFollowing: false
+    },
+    {
+        index: 2,
+        userName: 'justopancho',
+        name: 'Pancho Justo',
+        isFollowing: false
+    },
+    {
+        index: 3,
+        userName: 'TMChein',
+        name: 'Tomas',
+        isFollowing: false
+    }
+]
 
 export function App() {
     return (
-        <>
-            <TwitterFollowCard userName={"Juan Sifuentes"}
-                               image={"https://pbs.twimg.com/profile_images/1507177089123987458/IKF-yeje_400x400.jpg"}
-                               name={"Juan Sifuentes"}/>
-            <TwitterFollowCard userName={"Juan Sifuentes"}
-                               image={"https://pbs.twimg.com/profile_images/1507177089123987458/IKF-yeje_400x400.jpg"}
-                               name={"Juan Sifuentes"}/>
-            <TwitterFollowCard userName={"Juan Sifuentes"}
-                               image={"https://pbs.twimg.com/profile_images/1507177089123987458/IKF-yeje_400x400.jpg"}
-                               name={"Juan Sifuentes"}/>
-        </>
+        <section className={"App"}>
+            {
+                users.map(({ index, userName, name, isFollowing }) => (
+                        <TwitterFollowCard
+                            key={index}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                )
+            }
+        </section>
 
     )
 }
